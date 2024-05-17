d8756c6b4133dd21306bbbe54cba1838345299fd
Václav Stupka <vasstupka@seznam.cz>
2024-05-17 09:22:31 +0200

    basic interface (initialization and component adding)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 ﻿#include <iostream>
 #include <optional>
+#include <filesystem>
 
+#include "zip.hpp"
 #include "config.hpp"
+#include "download.hpp"
 
 using namespace std;
 using namespace oul;
@@ -11,31 +14,51 @@ int main(int argc, char* argv[])
 	vector<string> args(argv + 1, argv + argc);
 	if (argc > 1)
 	{
+		string config_file = find_configaration();
+
 		if (args[0] == "init")
 		{
 			initialize();
 		}
-	}
-	else
-	{
-		string config_file = find_configaration();
-
-		if (config_file == "")
+		else if (config_file == "")
 		{
 			cerr << "Configuration file not found." << endl;
-			return 0;
-		}
-
-		optional<CONFIG> cfg = read_configuration(config_file);
-		if (cfg.has_value())
-		{
-			cout << cfg->name;
+			return 1;
 		}
 		else
 		{
-			cerr << "Configuration file was corrupted." << endl;
-			return 0;
+			optional<CONFIG> c = read_configuration(config_file);
+			if (!c.has_value())
+			{
+				cerr << "Configuration file is corrupted." << endl;
+				return 1;
+			}
+			else if (args[0] == "add")
+			{
+				if (argc < 3)
+				{
+					cerr << "No component to download." << endl;
+					cerr << "Call this command with one additional argument." << endl;
+					return 2;
+				}
+
+				string url = c->default_url + "/" + args[1];
+				string component = download(url);
+
+				if (component != "")
+				{
+					vector<string> content = unzip(component, "");
+					add_component(url, component, content);
+					filesystem::remove(component);
+				}
+			}
 		}
+	}
+	else
+	{
+		cout << "Commands:" << endl;
+		cout << "    init    creates configuration file" << endl;
+		cout << "    add     downloads component and adds it into configuration" << endl;
 	}
 
 	return 0;
