6181e4e6bc01ea9c8a2eb4388397035c80069ad3
Václav Stupka <vasstupka@seznam.cz>
2024-08-15 23:19:21 +0200

    Read server settings to choose port.

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,45 @@
+import * as fs from "fs";
 import * as http from "http";
 import on_request from "./server";
 
-function on_listen(): void
+export type SERVER_CONFIG =
 {
-    console.log("Server se spustil.");
+    port: number,
+    get_requires_authorization: boolean,
+    post_requires_authorization: boolean,
+    users:
+    {
+        name: string
+        hashed_password: string
+    }[]
+}
+
+function on_listen(port: number): void
+{
+    console.log(`Server se spustil (na portu ${port}).`);
 }
 function on_server_error(error): void
  {
      console.warn(error.message);
  }
 
+function read_server_configuration(): SERVER_CONFIG
+{
+    try
+    {
+        const buf = fs.readFileSync("oul.server.json");
+        return JSON.parse(buf.toString());
+    }
+    catch (err)
+    {
+        console.warn(err.message);
+        process.exit(1);
+    }
+}
 function main(args: string[]): void
 {
-    let port = 8080;
+    const config = read_server_configuration();
+    let port = config.port;
     for (let arg of args)
     {
         if (arg.startsWith("-port"))
@@ -22,7 +49,7 @@ function main(args: string[]): void
     }
 
     const server = http.createServer(on_request);
-    server.listen(port, '127.0.0.1', on_listen);
+    server.listen(port, '127.0.0.1', ()=>on_listen(port));
     server.on("error", on_server_error);
 }
 
