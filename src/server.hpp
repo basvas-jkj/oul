e9fa27ba15f6b89cd96bf50aec408fba63fcfd79
Václav Stupka <vasstupka@seznam.cz>
2024-08-18 23:01:08 +0200

    Documentation for server.hpp

--- src/server.hpp
+++ src/server.hpp
@@ -5,6 +5,15 @@
 
 namespace oul
 {
+	/**
+	 * @brief Zaregistruje nového uživatele na serveru.
+	 * @param c Konfigurace projektu.
+	 **/
 	void registration(const CONFIG&);
+	/**
+	 * @brief Stáhne zazipovanou komponentu ze serveru.
+	 * @param url Adresa, ze které bude komponenta stažena
+	 * @return Cesta ke staženému archivu. Pokud stažení nebylo úspìšné, vrátí "".
+	 */
 	std::string download(const std::string&);
 }
