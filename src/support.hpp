ba8402fb54e4f55a5da550657a0e8b14aa80cf82
Václav Stupka <vasstupka@seznam.cz>
2024-08-25 20:27:51 +0200

    Documentation for support.cpp

--- src/support.hpp
+++ src/support.hpp
@@ -6,6 +6,16 @@
 namespace fs = boost::filesystem;
 namespace oul
 {
+    /**
+     * @brief Přesune soubor. Pokud cílová cesta neexistuje, bude vytvořena.
+     * @param source_directory původní umístění
+     * @param target_directory nové umístění
+     * @param entry_name jméno souboru či složky
+     **/
     void move(const fs::path& source_directory, const std::string& target_directory, const std::string& entry_name);
+    /**
+     * @brief Vytvoří složku pro dočasné soubory (podsložka oul v systémové složce).
+     * @return umístění složky pro dočasné soubory
+     **/
     fs::path get_temporary_folder();
 }
