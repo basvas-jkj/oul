0b286557b2119f80ff016ef474d9cdae8038efcd
Václav Stupka <vasstupka@seznam.cz>
2025-07-14 16:41:51 +0200

    Server umožňuje spustit na jiné IP než localhost.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
 import * as http from "http";
 import on_request from "./server";
 
-function on_listen(port: number): void
+function on_listen(ip: string, port: number): void
 {
-    console.log(`Server se spustil (na portu ${port}).`);
+    console.log(`Server se spustil (na ${ip}:${port}).`);
 }
 function on_server_error(error: Error): void
  {
@@ -13,17 +13,21 @@ function on_server_error(error: Error): void
 function main(args: string[]): void
 {
     let port = 8080;
+    let ip = '127.0.0.1';
     for (let arg of args)
     {
         if (arg.startsWith("-port"))
         {
             port = Number.parseInt(arg.split("=")[1]);
-            break;
+        }
+        else if (arg.startsWith("-ip"))
+        {
+            ip = arg.split("=")[1];
         }
     }
 
     const server = http.createServer((req, res)=>on_request(req, res));
-    server.listen(port, '127.0.0.1', ()=>on_listen(port));
+    server.listen(port, ip, ()=>on_listen(ip, port));
     server.on("error", on_server_error);
 }
 
